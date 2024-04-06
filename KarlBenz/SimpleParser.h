577765508a4f9f3c6ab93d4c825b01dfb1005f5c
Michael Gachanja <32077940+MikeGachanja@users.noreply.github.com>
2024-04-06 21:00:54 +0200

    Current Status

---
+++ KarlBenz/SimpleParser.h
@@ -0,0 +1,28 @@
+#ifndef SIMPLEPARSER_H_
+#define SIMPLEPARSER_H_
+
+#include <iostream>
+#include <sstream>
+#include <cstring>
+
+using namespace std;
+
+
+class SimpleParser
+{
+public:
+        static bool  parse(string sensors, string tag, float &value);
+
+        static bool  parse(string sensors, string tag, int &value);
+
+        static bool  parse(string sensors, string tag, float *value, int size);
+
+        static string stringify(string tag, float value);
+
+        static string  stringify(string tag, int value);
+
+        static string  stringify(string tag, float *value, int size);
+
+};
+
+#endif /*SIMPLEPARSER_H_*/
